f6a5673c8afcb298f895c7e938b6b1e688743c2d
Tatyana <Bagira-mur-rr@mail.ru>
2022-08-26 23:13:32 +0600

    добавлен цикл while (n == 0)

--- ex023/Program.cs
+++ ex023/Program.cs
@@ -1,12 +1,19 @@
-﻿Console.WriteLine("Введите число: ");
+﻿Console.WriteLine("Введите число отличное от нуля: ");
 int n = int.Parse(Console.ReadLine());
 int i = 1;
 int j = -1;
 
-Console.Write(n + " -> ");
 
+
+while (n == 0)
+{
+    Console.WriteLine("Введите число отличное от нуля: ");
+    n = int.Parse(Console.ReadLine());
+}
+Console.Write(n + "    -> ");
 if (n > 0)
 {
+    
     while (i <= n)
     {
         Console.Write(Math.Pow(i, 3) + ",");
@@ -15,9 +22,9 @@ if (n > 0)
 }
 else
 {
-   while (j >= n)
-    {
-        Console.Write(Math.Pow(j, 3) + ",");
+    while (j >= n)
+    {   
+        Console.Write(Math.Pow(j, 3) + ", ");
         j--;
-    } 
+    }
 }
