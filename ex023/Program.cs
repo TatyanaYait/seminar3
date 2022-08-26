0624aa593cf34602fe4ac102beac1f0504d08e27
Tatyana <Bagira-mur-rr@mail.ru>
2022-08-26 22:58:00 +0600

    таблица кубов от 1 до n, введенного с клавиатуры

---
+++ ex023/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+int i = 1;
+int j = -1;
+
+Console.Write(n + " -> ");
+
+if (n > 0)
+{
+    while (i <= n)
+    {
+        Console.Write(Math.Pow(i, 3) + ",");
+        i++;
+    }
+}
+else
+{
+   while (j >= n)
+    {
+        Console.Write(Math.Pow(j, 3) + ",");
+        j--;
+    } 
+}
